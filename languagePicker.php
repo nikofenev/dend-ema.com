982d7cd30eaad0c7106105d02c251ee5f7cecabb
Nikolay Fenev <nfenev@madisoncollege.edu>
2017-04-28 11:02:17 -0500

    Delete languagePicker.php

--- languagePicker.php
+++
@@ -1,4 +0,0 @@
-<div class="languagePicker">
-    <a href="indexBG.php"><img src="img/bg.png" widht="30px" height="30px"></a>
-    <a href="indexUS.php"><img src="img/us.png" widht="30px" height="30px"></a>
-</div>
